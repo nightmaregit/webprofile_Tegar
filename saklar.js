5497220cc8f3b910ce493644f8586dece17274df
nightmaregit <nightmaregit@gmail.com>
2023-09-26 16:01:28 +0700

    upload tugas profile

---
+++ saklar.js
@@ -0,0 +1,27 @@
+
+function saklar(kondisi){
+
+    let gam = document.getElementById("gambar");
+    let gam1 = document.getElementById("gambar1");
+    
+    //console.log(gam1);
+
+    if (kondisi == 0) {
+        gam.src = "off.gif";  
+    } 
+    if(kondisi == 1){
+        gam.src = "on.gif";
+    }
+    if (kondisi == 'b') {
+        gam1.src = "off.gif";
+    } 
+    if(kondisi == 'a'){
+        gam1.src = "on.gif";
+    }
+
+}
+
+
+  
+
+
